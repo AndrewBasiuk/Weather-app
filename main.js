6e31f3f430139d985515cff12c7c8395f1eeb683
Andrew Basiuk <a.basyuk4779@gmail.com>
2017-09-20 23:05:20 +0300

    temperature displayed on screen

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ $(document).ready(function() {
 	// var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q="Kiev"&&APPID=fc3da5f655d9b4c55ce7786120594255&units=metric";	
 
 	$(".form__button").click(function() {
-		var a = $(".form__input").val();
-		console.log(a);
+		var input = $(".form__input").val();
 
-		var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=" + a + "&&APPID=fc3da5f655d9b4c55ce7786120594255&units=metric";	
+		var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&&APPID=fc3da5f655d9b4c55ce7786120594255&units=metric";	
 		$.getJSON(weatherAPI, function(objektAPI) {
-			console.log(objektAPI.main);
+			var temp = objektAPI.main.temp;
+			$(".weather-info__temp").text(function() {
+				return "Temperature: " + temp + " °";
+			});
 		});
 	});
 
