623f22bc4b13395ca7aada6b1add35b7e53352be
Andrew Basiuk <a.basyuk4779@gmail.com>
2017-09-21 23:22:47 +0300

    add weather api for 5 days

--- main.js
+++ main.js
@@ -3,17 +3,26 @@
 
 $(document).ready(function() {
 
-	// var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q="Kiev"&&APPID=fc3da5f655d9b4c55ce7786120594255&units=metric";	
-
 	$(".form__button").click(function() {
 		var input = $(".form__input").val();
+		var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?q=London&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
 
-		var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&&APPID=fc3da5f655d9b4c55ce7786120594255&units=metric";	
 		$.getJSON(weatherAPI, function(objektAPI) {
-			var temp = objektAPI.main.temp;
-			$(".weather-info__temp").text(function() {
-				return "Temperature: " + temp + " °";
+			var dayList = objektAPI.list;
+			dayList.forEach( function(item, i) {
+				var temp = dayList[i*8+6].main.temp;
+				var date = dayList[i*8+6].dt_txt;
+				var el = "<li class='weather-info'>" + date + "</li>";
+				console.log(el);
+				// $('.weather-info').html(el);
 			});
+
+
+			// var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q="Kiev"&&APPID=fc3da5f655d9b4c55ce7786120594255&units=metric";	
+			// var temp = objektAPI.main.temp;
+			// $(".weather-info__temp").text(function() {
+			// 	return "Temperature: " + temp + " °";
+			// });
 		});
 	});
 
