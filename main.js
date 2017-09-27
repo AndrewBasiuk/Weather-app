df8e60909f98a412316d8a5714c8211ce69ae861
Andrew Basiuk <a.basyuk4779@gmail.com>
2017-09-27 22:08:45 +0300

    change design and add weather now

--- main.js
+++ main.js
@@ -6,19 +6,25 @@ $(document).ready(function() {
 	$(".form__button").click(function() {
 		var input = $(".form__input").val();
 		var inputBigLetter = input.charAt(0).toUpperCase() + input.slice(1);
-		var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
+		var weatherFiveDays = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
+		var weatherNow = "http://api.openweathermap.org/data/2.5/weather?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
 
 		$('.weather-info__heading').text(inputBigLetter + " weather for 5 days");
 		$('.weather-info__item').remove();
 
-		$.getJSON(weatherAPI, function(objektAPI) {
+		$.getJSON(weatherNow, function(objektAPI) {
+			var temp = objektAPI.main.temp;
+			$('.weather-indo__now').text("Now in " + inputBigLetter + "  " + temp + "°");
+			console.log(objektAPI);
+		});
+
+		$.getJSON(weatherFiveDays, function(objektAPI) {
 			var dayList = objektAPI.list;
 
 			dayList.forEach( function(item, i) {
 				var temp = Math.round(dayList[i*8+6].main.temp),
 					date = dayList[i*8+6].dt_txt.slice(0, 10),
 					el = "<li class='weather-info__item'><p class='weather-info__paragraph'>" + date + " is " + temp + "°" + "</p></li>";
-				console.log(date);
 				return $('.weather-info-list').append(el);
 			});
 		});
