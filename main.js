6104555f75ce3005d8c5f9c19eb527475b1eb0b2
Andrew Basiuk <a.basyuk4779@gmail.com>
2017-09-27 21:23:44 +0300

    add rewrite temperature for new city; add list heading

--- main.js
+++ main.js
@@ -5,20 +5,22 @@ $(document).ready(function() {
 
 	$(".form__button").click(function() {
 		var input = $(".form__input").val();
-		var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?q=London&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
+		var inputBigLetter = input.charAt(0).toUpperCase() + input.slice(1);
+		var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
+
+		$('.weather-info__heading').text(inputBigLetter + " weather for 5 days");
+		$('.weather-info__item').remove();
 
 		$.getJSON(weatherAPI, function(objektAPI) {
 			var dayList = objektAPI.list;
 
-			var dateList = dayList.forEach( function(item, i) {
+			dayList.forEach( function(item, i) {
 				var temp = Math.round(dayList[i*8+6].main.temp),
 					date = dayList[i*8+6].dt_txt.slice(0, 10),
 					el = "<li class='weather-info__item'><p class='weather-info__paragraph'>" + date + " is " + temp + "°" + "</p></li>";
-
-				return $('.weather-info').append(el);
-				
+				console.log(date);
+				return $('.weather-info-list').append(el);
 			});
-
 		});
 	});
 
