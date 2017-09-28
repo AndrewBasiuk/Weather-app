f97fe1ea3a0869aeb11d6c0d5d6f98e3bf3028cd
Andrew Basiuk <a.basyuk4779@gmail.com>
2017-09-28 23:24:42 +0300

    little style fix

--- main.js
+++ main.js
@@ -3,42 +3,55 @@
 
 $(document).ready(function() {
 
-	$(".form__button").click(function() {
-		var input = $(".form__input").val();
-		var inputBigLetter = input.charAt(0).toUpperCase() + input.slice(1);
-		var weatherFiveDays = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
-		var weatherNow = "http://api.openweathermap.org/data/2.5/weather?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
+	// $(".form__button").click(function() {
+	// 	var input = $(".form__input").val(),
+	// 		inputBigLetter = input.charAt(0).toUpperCase() + input.slice(1),
+	// 		weatherFiveDays = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric",
+	// 		weatherNow = "http://api.openweathermap.org/data/2.5/weather?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
 
-		$('.weather-info__heading').text(inputBigLetter + " weather for 5 days");
-		$('.weather-info__item').remove();
-		$('.weather-info-now__image').remove();
+	// 	$('.weather-info__heading').text(inputBigLetter + " weather for 5 days");
+	// 	$('.weather-info__item').remove();
+	// 	$('.weather-info-now__image').remove();
 
-		$.getJSON(weatherNow, function(objektAPI) {
-			var temp = Math.round(objektAPI.main.temp);
-			var weatherIcon = objektAPI.weather[0].icon;
-			var imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+	// 	$.getJSON(weatherNow, function(objektAPI) {
+	// 		var temp = Math.round(objektAPI.main.temp),
+	// 			weatherIcon = objektAPI.weather[0].icon,
+	// 			imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+
+	// 		$('.weather-indo-now__text').text("Now in " + inputBigLetter + "  " + temp + "°");
+	// 		$('.weather-indo-now').append("<img src=" + imgLink + " alt='icon' class='weather-info-now__image'>");
+	// 	});
 
 
-			$('.weather-indo-now__text').text("Now in " + inputBigLetter + "  " + temp + "°");
-			$('.weather-indo-now').append("<img src=" + imgLink + " alt='icon' class='weather-info-now__image'>");
-		});
+	// 	$.getJSON(weatherFiveDays, function(objektAPI) {
+	// 		var dayList = objektAPI.list;
 
-		$.getJSON(weatherFiveDays, function(objektAPI) {
-			var dayList = objektAPI.list;
+	// 		dayList.forEach( function(item, i) {
+	// 			var temp = Math.round(dayList[i*8+6].main.temp),
+	// 				 date = dayList[i*8+6].dt_txt.slice(0, 10),
+	// 				 weatherIcon = dayList[i*8+6].weather[0].icon,
+	// 				 imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png",
+	// 				 paragraph = "<p class='weather-info__paragraph'>" + date + " is " + temp + "°" + "</p>",
+	// 				 image = "<img src=" + imgLink + " alt='icon' class='weather-info__image'>",
+	// 				 el = "<li class='weather-info__item'>" + paragraph + image + "</li>";
 
-			dayList.forEach( function(item, i) {
-				var temp = Math.round(dayList[i*8+6].main.temp),
-					 date = dayList[i*8+6].dt_txt.slice(0, 10),
-					 weatherIcon = dayList[i*8+6].weather[0].icon,
-					 imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png",
-					 paragraph = "<p class='weather-info__paragraph'>" + date + " is " + temp + "°" + "</p>",
-					 image = "<img src=" + imgLink + " alt='icon' class='weather-info__image'>",
-					 el = "<li class='weather-info__item'>" + paragraph + image + "</li>";
-					 console.log(weatherIcon);
-				return $('.weather-info-list').append(el);
-			});
-		});
-	});
+	// 			return $('.weather-info-list').append(el);
+	// 		});
+	// 	});
+	// });
+
+	var date = new Date();
+
+	var options = {
+	  year: 'numeric',
+	  month: 'long',
+	  day: 'numeric',
+	  weekday: 'long',
+	  timezone: 'UTC'
+	};
+
+	alert( date.toLocaleString("en-US", options) );
+
 
 
 });
