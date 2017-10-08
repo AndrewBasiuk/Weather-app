18e661671ba26b8a6671efb0c74ea190a848a977
Andrew Basiuk <a.basyuk4779@gmail.com>
2017-10-08 23:59:52 +0300

    write code on native JS

--- main.js
+++ main.js
@@ -1,57 +1,134 @@
+function showWeather(e) {
+	e.preventDefault();
+
+	var input = document.getElementById('form__input').value,
+	inputBigLetter = input.charAt(0).toUpperCase() + input.slice(1),
+	weatherFiveDays = 'http://api.openweathermap.org/data/2.5/forecast?q=' + inputBigLetter + '&appid=fc3da5f655d9b4c55ce7786120594255&units=metric',
+	weatherNow = "http://api.openweathermap.org/data/2.5/weather?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
 
 
+	axios.get(weatherNow)
+	  .then(function (response) {
+	  	var objektAPI = response.data;
 
-$(document).ready(function() {
+	    var temp = Math.round(objektAPI.main.temp),
+			weatherIcon = response.data.weather[0].icon,
+			imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
 
-	function showWeather() {
-		var input = $(".form__input").val(),
-			inputBigLetter = input.charAt(0).toUpperCase() + input.slice(1),
-			weatherFiveDays = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric",
-			weatherNow = "http://api.openweathermap.org/data/2.5/weather?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
+		var weatherInfoNow = document.getElementById('weather-info-now');
+		var weatherInfoNowText = document.getElementById('weather-info-now__text');
+		var weatherInfoNowImage = document.createElement('img');
+		weatherInfoNowImage.className = "weather-info-now__image";
+		weatherInfoNowImage.setAttribute('src', imgLink);
 
-		$('.weather-info__heading').text(inputBigLetter + " weather for 5 days");
-		$('.weather-info__item').remove();
-		$('.weather-info-now__image').remove();
 
-		$.getJSON(weatherNow, function(objektAPI) {
-			var temp = Math.round(objektAPI.main.temp),
-				weatherIcon = objektAPI.weather[0].icon,
+		weatherInfoNowText.innerHTML = "Now in " + inputBigLetter + "  " + temp + "°";
+		weatherInfoNow.appendChild(weatherInfoNowImage);
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  });
+
+
+	axios.get(weatherFiveDays)
+	  .then(function(response) {
+	  	var dayList = response.data.list;
+
+	  	dayList.forEach(function(item, i) {
+	  		var selection = i*8+6;
+
+			var temp = Math.round(dayList[selection].main.temp);
+				date = dayList[selection].dt_txt.slice(0, 10);
+				weatherIcon = dayList[selection].weather[0].icon,
 				imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
 
-			$('.weather-indo-now__text').text("Now in " + inputBigLetter + "  " + temp + "°");
-			$('.weather-indo-now').append("<img src=" + imgLink + " alt='icon' class='weather-info-now__image'>");
-		});
+			var weatherInfoList = document.getElementById('weather-info-list');
+
+			var weatherInfoListItem = document.createElement('li');
+			weatherInfoListItem.className = "weather-info__item";
+
+			var weatherInfoDate = document.createElement('p');
+			weatherInfoDate.className = "weather-info__date";
+			weatherInfoDate.innerHTML = date;
+
+			var weatherInfoTemp = document.createElement('p');
+			weatherInfoTemp.className = "weather-info__temp";
+			weatherInfoTemp.innerHTML = temp + "°";
+
+			var weatherInfoImage = document.createElement('img');
+			weatherInfoImage.setAttribute("src", imgLink);
+			weatherInfoImage.className = "weather-info__image";
+
+			var weatherInfoValue = document.createElement('div');
+			weatherInfoValue.className = "weather-info__value";
+			weatherInfoValue.appendChild(weatherInfoTemp);
+			weatherInfoValue.appendChild(weatherInfoImage);
+
+			weatherInfoList.appendChild(weatherInfoListItem);
+			weatherInfoListItem.appendChild(weatherInfoDate);
+			weatherInfoListItem.appendChild(weatherInfoValue);
+
+	  	});
+	  })
+
+	  .catch(function(error) {
+	  	console.log(error);
+	  })
+
+};
+
+var form = document.getElementById('form');
+form.addEventListener('submit', showWeather, false);
 
 
-		$.getJSON(weatherFiveDays, function(objektAPI) {
-			var dayList = objektAPI.list;
-
-			dayList.forEach( function(item, i) {
-				var temp = Math.round(dayList[i*8+6].main.temp),
-					 date = dayList[i*8+6].dt_txt.slice(0, 10),
-					 weatherIcon = dayList[i*8+6].weather[0].icon,
-					 imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png",
-					 dateParagraph = "<p class='weather-info__date'>" + date + "</p>",
-					 tempParagraph = "<p class='weather-info__paragraph'>" + temp +  "°" + "</p>",
-					 image = "<img src=" + imgLink + " alt='icon' class='weather-info__image'>",
-					 weatherValue = "<div class='weather-info__value'>" + tempParagraph + image + "</div>",
-					 el = "<li class='weather-info__item'>" + dateParagraph + weatherValue + "</li>";
-
-				return $('.weather-info-list').append(el);
-			});
-		});
-	}
-
-	$(".form__button").click(function() {
-		showWeather();
-	});
-
-	$(".form__input").keypress(function(e){
-	   if(e.keyCode==13){
-	     	showWeather();
-	   }
-	});
 
 
+// $(document).ready(function() {
 
-});
+// 	function showWeather() {
+// 		var input = $(".form__input").val(),
+// 			inputBigLetter = input.charAt(0).toUpperCase() + input.slice(1),
+// 			weatherFiveDays = 'http://api.openweathermap.org/data/2.5/forecast?q=' + inputBigLetter + '&appid=fc3da5f655d9b4c55ce7786120594255&units=metric',
+// 			weatherNow = "http://api.openweathermap.org/data/2.5/weather?q=" + inputBigLetter + "&appid=fc3da5f655d9b4c55ce7786120594255&units=metric";
+
+// 		$('#weather-info__heading').text(inputBigLetter + " weather for 5 days");
+// 		$('.weather-info__item').remove();
+// 		$('#weather-info-now__image').remove();
+
+// 		$.getJSON(weatherNow, function(objektAPI) {
+// 			var temp = Math.round(objektAPI.main.temp),
+// 				weatherIcon = objektAPI.weather[0].icon,
+// 				imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+
+// 			$('#weather-info-now__text').text("Now in " + inputBigLetter + "  " + temp + "°");
+// 			$('#weather-info-now').append("<img src=" + imgLink + " alt='icon' id='weather-info-now__image' class='weather-info-now__image'>");
+// 		});
+
+
+// 		$.getJSON(weatherFiveDays, function(objektAPI) {
+// 			var dayList = objektAPI.list;
+
+// 			dayList.forEach( function(item, i) {
+// 				var selection = i*8+6;
+
+// 				var temp = Math.round(dayList[selection].main.temp),
+// 					 date = dayList[selection].dt_txt.slice(0, 10),
+// 					 weatherIcon = dayList[selection].weather[0].icon,
+// 					 imgLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png",
+// 					 dateParagraph = "<p class='weather-info__date'>" + date + "</p>",
+// 					 tempParagraph = "<p class='weather-info__paragraph'>" + temp +  "°" + "</p>",
+// 					 image = "<img src=" + imgLink + " alt='icon' class='weather-info__image'>",
+// 					 weatherValue = "<div class='weather-info__value'>" + tempParagraph + image + "</div>",
+// 					 el = "<li id='weather-info__item' class='weather-info__item'>" + dateParagraph + weatherValue + "</li>";
+
+// 				return $('#weather-info-list').append(el);
+// 			});
+// 		});
+// 	}
+
+// 	$("form").submit(function(e) {
+// 		showWeather();
+// 		e.preventDefault();
+// 	});
+
+// });
